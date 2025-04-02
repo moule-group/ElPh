Fix the error of the code, add new module for variance to run SVD

src/elph/elph.py
@@ -312,7 +312,7 @@ def get_deri_Jmatrix(j_list, delta=0.01):
 
     return matrix
 
-def variance(freqs, g, qpts, temp):
+def variance(freqs, g, qpts, temp, svd=False):
     """ Calculate the variance of the transfer integral J
     Apply the formula: Var(J) = <J^2> - <J>^2
     Args:
@@ -320,7 +320,11 @@ def variance(freqs, g, qpts, temp):
     g: electron-phonon coupling matrix
     qpts (int): total kpts used in phonon calculation
     temp (float): Temperture in Kelvin
+    svd (bool): If True, need to make freqs array into (x, 3) shape
     """
+    if svd:
+        freqs = np.tile(freqs[:, np.newaxis], (1, 3))
+        
     var = (g**2/2)/np.tanh((hbar*freqs*1e12)/(2*k*temp)) # freqs in Phonopy is THz, so need to convert to Hz
     sigma = (np.sum(var)/qpts)**0.5 # Square root of variance, have to do normalization over the number of q points 
     return var, sigma

src/elph/svdprojection.py
@@ -3,21 +3,21 @@ import yaml
 #from qutip import * # quantum toolbox in python
 
 
-def svd_projection(matrix, num_modes, nqpts, threshold=1e-9):
+def svd_projection(num_modes, nqpts, threshold=1e-9, matrix='epc'):
     """
     Singular value decomposition (SVD) of the electron-phonon coupling matrix
     and projection of the phonon modes into system and bath modes.
 
     Parameters
     ----------
-    matrix : str
-        Select the matrix to be used for SVD, can be 'epc' or 'variance'.
     num_modes : int
         Number of phonon modes.
     nqpts : int
         Number of q-points.
     threshold : float
         Threshold for singular values to be considered zero.
+    matrix : str
+        Select the matrix to be used for SVD, can be 'epc' or 'variance'.
 
     Returns
     -------
@@ -41,18 +41,17 @@ def svd_projection(matrix, num_modes, nqpts, threshold=1e-9):
 
     # Numpy SVD 
     if matrix == 'epc':
-        # Load epcoupling 
-        cp = np.load('svd_ep_coupling.npz')
+        cp = np.load('svd_ep_coupling.npz') # Load epc
         epcA = cp['A'][0:num_modes*nqpts]
         epcB = cp['B'][0:num_modes*nqpts]
         epcC = cp['C'][0:num_modes*nqpts]
         epc = epcA+epcB+epcC
         print(f"EPC shape is {epc.shape}")
         U, S, Vh = np.linalg.svd(epc, full_matrices=True)  # Reduced SVD (if full_matrices is False): U is rotational orthogonal matrix; 
+        # S is singular vectors (return singular value in 1D array); Vh is rotational orthogonal matrix
     
     if matrix == 'variance':
-        # Load variance
-        variance = np.load('variance_for_svd.npz')
+        variance = np.load('variance_for_svd.npz') # Load variance
         varA = variance['A'][0:num_modes*nqpts]
         varB = variance['B'][0:num_modes*nqpts]
         varC = variance['C'][0:num_modes*nqpts]
@@ -60,7 +59,7 @@ def svd_projection(matrix, num_modes, nqpts, threshold=1e-9):
         print(f"Variance shape is {var.shape}")
         U, S, Vh = np.linalg.svd(var, full_matrices=True)
     
-    print(f'Singular values are {S}') #S is singular vectors (return singular value in 1D array); Vh is rotational orthogonal matrix
+    print(f'Singular values are {S}') 
     print(f"Shape of left orthogonal matrix {U.shape}")
     print(f"Shape of singular vectors {S.shape}")
     print(f"Shape of right orthogonal matrix {Vh.shape}")
@@ -69,7 +68,7 @@ def svd_projection(matrix, num_modes, nqpts, threshold=1e-9):
     print('Indices of non-zero singular values=', Snonzero)
 
     P = U[:, Snonzero] @ U[:, Snonzero].T # Projection operator
-    I = np.eye(num_modes * qpts)
+    I = np.eye(num_modes * nqpts)
     Q = I - P # Complement operator
     print(f"Shape of projection operator {P.shape}")
     print(f"Projection operator test: ||P^2 - P|| = {np.linalg.norm(P @ P - P)}")

src/elph/workflow.py
@@ -226,9 +226,9 @@ def run_matrix(mesh,sc):
                 'vC':variC,
                 'sC':sigmaC}
 
-    svd_variA, _ = ep.variance(freqs, svd_epcA, nqpts, 298) # Variance for dimer A
-    svd_variB, _ = ep.variance(freqs, svd_epcB, nqpts, 298) # Variance for dimer B
-    svd_variC, _ = ep.variance(freqs, svd_epcC, nqpts, 298) # Variance for dimer C
+    svd_variA, _ = ep.variance(freqs, svd_epcA, nqpts, 298, svd=True) # Variance for further SVD projection
+    svd_variB, _ = ep.variance(freqs, svd_epcB, nqpts, 298, svd=True) 
+    svd_variC, _ = ep.variance(freqs, svd_epcC, nqpts, 298, svd=True) 
 
     svd_variance = {'vA':svd_variA,
                     'vB':svd_variB,  
@@ -263,9 +263,9 @@ def run_svd_projection(matrix, nqpts):
     main_path = os.getcwd()
     atoms = ase.io.read(getGeometry(main_path)) # Read the structure file
     natoms = len(atoms)
-    num_modes = 3 * natoms
+    nmodes = 3 * natoms
     if matrix == 'epc':
-        svd_epcA, svd_epcB, svd_epcC, f_sys, f_bath, coeff_sys, coeff_bath = svd.svd_projection(matrix, num_modes, nqpts)
+        svd_epcA, svd_epcB, svd_epcC, f_sys, f_bath, coeff_sys, coeff_bath = svd.svd_projection(num_modes=nmodes, nqpts=nqpts)
 
         svd_epc = {'A':svd_epcA,
                    'B':svd_epcB,
@@ -278,7 +278,7 @@ def run_svd_projection(matrix, nqpts):
         np.savez_compressed('svd_epc_result' + '.npz', **svd_epc) 
 
     if matrix == 'variance':
-        svd_varA, svd_varB, svd_varC, f_sys, f_bath, coeff_sys, coeff_bath = svd.svd_projection(matrix, num_modes, nqpts)
+        svd_varA, svd_varB, svd_varC, f_sys, f_bath, coeff_sys, coeff_bath = svd.svd_projection(um_modes=nmodes, nqpts=nqpts, matrix='variance')
 
         svd_epc = {'A':svd_varA,
                    'B':svd_varB,
@@ -289,19 +289,6 @@ def run_svd_projection(matrix, nqpts):
                    'coeff_bath':coeff_bath}
     
         np.savez_compressed('svd_variance_result' + '.npz', **svd_epc)
-
-    #svd_variA, svd_sigmaA = ep.variance(f_sys, svd_epcA, nqpts, 298) # Variance for dimer A
-    #svd_variB, svd_sigmaB = ep.variance(f_sys, svd_epcB, nqpts, 298) # Variance for dimer B
-    #svd_variC, svd_sigmaC = ep.variance(f_sys, svd_epcC, nqpts, 298) # Variance for dimer C
-
-    #svd_variance = {'vA':svd_variA,
-     #               'sA':svd_sigmaA,
-     #               'vB':svd_variB,
-     #               'sB':svd_sigmaB,
-     #               'vC':svd_variC,
-     #               'sC':svd_sigmaC}
-
-    #np.savez_compressed('svd_variance' + '.npz', **svd_variance)
     
    
     